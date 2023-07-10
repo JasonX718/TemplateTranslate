36a002bd6a687f89c6c9a297fb58e16cee4f941d
XIAO <jayxiao0718@gmail.com>
2023-07-10 04:52:41 +0200

    init git

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "Alarm limit on Analog Input";
+            if(str.Contains("Alarm on"))
+            {
+                Console.WriteLine(1);
+            }
+            else
+            {
+                Console.WriteLine(0);
+            }
+        }
+    }
+}
+;
